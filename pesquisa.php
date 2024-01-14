c72241f0e881b0ee6680ef1a4555b91b03d6dfb3
lazariniuser <gabriellazarinicontato@gmail.com>
2024-01-14 14:42:08 -0300

    campo de pesquisas

---
+++ pesquisa.php
@@ -0,0 +1,16 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $dbname = "funildevendas";
+    //Criar a conexao
+    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+    
+    $pesquisar = $_POST['pesquisar'];
+    $result_cursos = "SELECT * FROM cursos WHERE nome LIKE '%$pesquisar%' LIMIT 5";
+    $resultado_cursos = mysqli_query($conn, $result_cursos);
+    
+    while($rows_cursos = mysqli_fetch_array($resultado_cursos)){
+        echo "Nome do curso: ".$rows_cursos['nome']."<br>";
+    }
+?>
