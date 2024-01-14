8316e08ec64a67c2f74934d92c49a8a7ca2b9894
lazariniuser <gabriellazarinicontato@gmail.com>
2024-01-14 14:57:46 -0300

    arrumando campo pesquisa

--- pesquisa.php
+++ pesquisa.php
@@ -2,7 +2,8 @@
 
     include "connect.php";
 
-    $query_pesquisa = "SELECT * FROM func WHERE nome LIKE ". $campo_pesquisa ." LIMIT 3";
+    $campo_pesquisa = $_POST'campo_pesquisa';
+    $query_pesquisa = "SELECT * FROM func WHERE nome LIKE  '%$campo_pesquisa%'  LIMIT 3";
     $pdo_pesquisa = $pdo->query($query_pesquisa);
     while ($pesquisa = $pdo_pesquisa->fetch(PDO::FETCH_ASSOC)) {
     
