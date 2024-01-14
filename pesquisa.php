1cc55b5e8a695308326be201a6826a1d66c8b48e
lazariniuser <gabriellazarinicontato@gmail.com>
2024-01-14 14:54:47 -0300

    correção campo pesquisa

--- pesquisa.php
+++ pesquisa.php
@@ -2,7 +2,7 @@
 
     include "connect.php";
 
-    $query_pesquisa = "SELECT * FROM func WHERE nome LIKE "%$campo_pesquisa%" LIMIT 3";
+    $query_pesquisa = "SELECT * FROM func WHERE nome LIKE ". $campo_pesquisa ." LIMIT 3";
     $pdo_pesquisa = $pdo->query($query_pesquisa);
     while ($pesquisa = $pdo_pesquisa->fetch(PDO::FETCH_ASSOC)) {
     
